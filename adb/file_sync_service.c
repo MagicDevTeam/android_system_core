aa16f36cf26649baf0cedb559219ecc96dfc3b30
SunRain <41245110@qq.com>
2014-01-29 20:25:23 +0800

    adb: configure /system file permission with fs_config
    adbd changes the permissions of files uploaded by copying
    owner's permission to group and other. This causes kernel
    load failure if any changes are made to kernel modules
    followed by ``adb push'' or ``adb sync'' because file mode
    644 would become 666.
    
    The change uses fs_config to get the target mode of files.
    
    In addition, the SELinux file label is set incorrectly
    on synced files. Set the label properly.
    
    Change-Id: I354db848f39adfbf189d7e8d3b028e8d763c772f
    Signed-off-by: Liang Cheng <licheng@nvidia.com>
    Bug: 12264775

--- adb/file_sync_service.c
+++ adb/file_sync_service.c
@@ -40,15 +40,6 @@ static bool is_on_system(const char *name) {
     return (strncmp(SYSTEM, name, strlen(SYSTEM)) == 0);
 }
 
-static bool has_group_sdcard(const char *path) {
-    const char *SDCARD = "sdcard";
-    struct stat info;
-    if(stat(path, &info) != 0)
-        return 0;
-    struct group *gr = getgrgid(info.st_gid);
-    return (strstr(gr->gr_name, SDCARD) != NULL);
-}
-
 static int mkdirs(char *name)
 {
     int ret;
@@ -74,12 +65,10 @@ static int mkdirs(char *name)
             *x = '/';
             return ret;
         } else if(ret == 0) {
-            if(!has_group_sdcard(name)) {
-                ret = chown(name, uid, gid);
-                if (ret < 0) {
-                    *x = '/';
-                    return ret;
-                }
+            ret = chown(name, uid, gid);
+            if (ret < 0) {
+                *x = '/';
+                return ret;
             }
             selinux_android_restorecon(name);
         }
@@ -207,7 +196,7 @@ static int handle_send_file(int s, char *path, unsigned int uid,
         if(fail_errno(s))
             return -1;
         fd = -1;
-    } else if(!has_group_sdcard(path)) {
+    } else {
         if(fchown(fd, uid, gid) != 0) {
             fail_errno(s);
             errno = 0;
